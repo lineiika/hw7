743aee2d27133d619c30d48e73dfea0c6ee35daa
lineiika <avramenko.liana@gmail.com>
2023-01-18 16:55:24 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,63 @@
+// 1
+// let str = '20'
+// for (let i = 20.5; i <= 30; i = i+ 0.5){
+//     str = str + ' '+ i;
+// }
+// console.log(str);
+
+// 2 
+// for (let i=27; i<=27; i++) { 
+//          let j = 10;
+      
+//     while (j<=100) {
+//          document.write(i + " * " + j + " = " + i*j + "<br>");
+//           j= j+10;
+//         }
+// }
+
+// 3
+
+// let userAnswer = Number(prompt('Enter your number'));
+
+// for( let i = 1; i <= 100; i++){
+//     if(i*i < userAnswer){
+//         console.log(`${i}`);
+//     }
+    
+// }
+// 4
+
+// let userAnswer = Number(prompt('Enter your number'));
+
+// for (let i = 2; i < userAnswer; i ++){
+//         if (userAnswer % i === 0){
+//             console.log("Your number is not natural");
+//         }
+        
+//         console.log ("Your number is natural");
+// }
+
+// 5
+
+
+// let n = 81
+// for(let i= 1; i<=10;i++){
+//     let a = 3 ** i;
+//     if( n===a){
+//         console.log('Number is in power '+ n); break;
+//     }
+// }
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
